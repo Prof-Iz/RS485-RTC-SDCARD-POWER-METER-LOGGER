75e7b9e728e4d0e7dc933d93ec9292a64b1f4054
Izdhan <57706976+Prof-Iz@users.noreply.github.com>
2020-09-24 09:39:19 +0800

    debugging RS485 option

--- logging/logging.ino
+++ logging/logging.ino
@@ -26,9 +26,6 @@ File data;
 ModbusMaster node;
 
 char filename[12]; //to store the updating filename
-//char debug_time[20];
-
-int year_rtc;
 
 int ID = 1; // Slave ID
 uint8_t result;
@@ -71,9 +68,9 @@ void setup()
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
-  //  node.begin(ID, Serial);
-  //  node.preTransmission(preTransmission);
-  //  node.postTransmission(postTransmission);
+  node.begin(ID, Serial);
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
 }
 
 void loop()
@@ -88,64 +85,53 @@ void loop()
   }
 
   data = SD.open(filename, FILE_WRITE);
-  Serial.println(data);
 
   if (data)
   {
     // req and write modbus
+    Serial.println("File made");
 
-    //    do
-    //    { // give 2 chances to get proper reading.
-    //      result = node.readHoldingRegisters(0x3911, 5); //read 5 registers from 0x3911 onwards, function code 03
-    //      //    freq = node.read(0x3915); // check if reading seperately still accurate enough
-    //      //    VLN = node.read(0x3911);
-    //      error_counter--;
-    //    } while ((!node.ku8MBSuccess) and (error_counter > 0));
+    do
+    { // give 2 chances to get proper reading.
+      result = node.readHoldingRegisters(0x3911, 5); //read 5 registers from 0x3911 onwards, function code 03
+      //    freq = node.read(0x3915); // check if reading seperately still accurate enough
+      //    VLN = node.read(0x3911);
+      error_counter--;
+      Serial.print(error_counter);
+      Serial.println(" tries left");
+    } while ((!node.ku8MBSuccess) and (error_counter > 0));
 
     error_counter = 2; //reset error counter
 
-    //    if (node.ku8MBSuccess)
-    //    {
-    //      data.print(now.hour());
-    //      data.print(":");
-    //      data.print(now.minute());
-    //      //      00:00
-    //
-    //      data.print(",");
-    //      data.print(node.getResponseBuffer(0x00)); // VLN
-    //      data.print(",");
-    //      data.println(node.getResponseBuffer(0x04)); // Freq
-    //    }
+    if (node.ku8MBSuccess)
+    {
+      Serial.println("Data read a success");
+//      data.print(now.hour());
+//      data.print(":");
+//      data.print(now.minute());
+//      //      00:00:00
+//
+//      data.print(",");
+//      data.print(node.getResponseBuffer(0x00)); // VLN
+//      data.print(",");
+//      data.println(node.getResponseBuffer(0x04)); // Freq
+
+      Serial.print(now.hour());
+      Serial.print(":");
+      Serial.print(now.minute());
+      //      00:00:00
+
+      Serial.print(",");
+      Serial.print(node.getResponseBuffer(0x00)); // VLN
+      Serial.print(",");
+      Serial.println(node.getResponseBuffer(0x04)); // Freq
+    }
 
     // debugging
-
-    
-    data.print(now.hour());
-    data.print(":");
-    data.print(now.minute());
-    data.print(":");
-    data.print(now.second());
-    //      00:00
-
-    data.print(",");
-    data.println(random());
-
-
-    Serial.print(now.hour());
-    Serial.print(":");
-    Serial.print(now.minute());
-    Serial.print(":");
-    Serial.print(now.second());
-    //      00:00
-
-    Serial.print(",");
-    Serial.println(random());
-
 
 
 
     data.close(); // to ensure that its saved properly
-    Serial.println(data);
   }
-  delay(3000); // a minute delay
+  delay(10000); // a minute delay
 }
