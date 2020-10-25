4d1c0a7c0ce78011fa0e95b4978711e1e428cb4d
Izdhan <57706976+Prof-Iz@users.noreply.github.com>
2020-10-26 06:22:47 +0800

    updates

--- logging/logging.ino
+++ logging/logging.ino
@@ -13,7 +13,7 @@
     Physical Hardware uses DS1307.
     Double check whether the battery is CR1220 3V.
     Format SD Card to FAT16
-    Modbus Master by Walker
+    Modbus Master by Doc Walker
 */
 
 #define MAX485_DE 3
@@ -21,19 +21,23 @@
 
 RTC_DS1307 rtc;
 
-File data;
+File logger;
 
 ModbusMaster node;
 
 char filename[12]; //to store the updating filename
 
 int ID = 1; // Slave ID
+
 uint8_t result;
 
-//uint8_t freq; //store result from modbus
-//uint8_t VLN;
-
 int error_counter = 2;
+
+uint16_t data[2];
+
+uint32_t value = 0;
+
+double reading = 0;
 
 void preTransmission()
 {
@@ -47,10 +51,25 @@ void postTransmission()
   digitalWrite(MAX485_DE, 0);
 }
 
+float HexToFloat(uint32_t x) { //cconvert Hex uint32_t value to float under IEEE 754
+
+  return (*(float*)&x);
+}
+
 void setup()
 {
+  //allow enable pins to be output
+  pinMode(MAX485_RE_NEG, OUTPUT);
+  pinMode(MAX485_DE, OUTPUT);
+  // Init in receive mode
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+
+
   EEPROM.write(0, 0); // stores the day of the month, start with zero so setup is okay
-  Serial.begin(9600); // check if baudrate functions properly.
+
+
+  Serial.begin(9600, SERIAL_8E1); // baudrate 9600, 8bit, Even Parity, 1 Stop bit
   //   according to docs noisy, EMI, RFI environments should run 4800, 2400 baud.
 
   while (!Serial)
@@ -84,16 +103,16 @@ void loop()
     EEPROM[0] = now.day();
   }
 
-  data = SD.open(filename, FILE_WRITE);
+  logger = SD.open(filename, FILE_WRITE);
 
-  if (data)
+  if (logger)
   {
     // req and write modbus
     Serial.println("File made");
 
     do
     { // give 2 chances to get proper reading.
-      result = node.readHoldingRegisters(0x3911, 5); //read 5 registers from 0x3911 onwards, function code 03
+      result = node.readHoldingRegisters(0x3030, 20); //read 5 registers from 0x3911 onwards, function code 03
       //    freq = node.read(0x3915); // check if reading seperately still accurate enough
       //    VLN = node.read(0x3911);
       error_counter--;
@@ -103,28 +122,17 @@ void loop()
 
     error_counter = 2; //reset error counter
 
-    if (node.ku8MBSuccess)
+    if (result == node.ku8MBSuccess)
     {
-      Serial.println("Data read a success");
-//      data.print(now.hour());
-//      data.print(":");
-//      data.print(now.minute());
-//      //      00:00:00
-//
-//      data.print(",");
-//      data.print(node.getResponseBuffer(0x00)); // VLN
-//      data.print(",");
-//      data.println(node.getResponseBuffer(0x04)); // Freq
 
-      Serial.print(now.hour());
-      Serial.print(":");
-      Serial.print(now.minute());
-      //      00:00:00
 
-      Serial.print(",");
-      Serial.print(node.getResponseBuffer(0x00)); // VLN
-      Serial.print(",");
-      Serial.println(node.getResponseBuffer(0x04)); // Freq
+      logger.print(now.hour());
+      logger.print(":");
+      logger.print(now.minute());
+      //      00:00
+      logger.print(",");
+
+
     }
 
     // debugging
--- modbus_portion/modbus_portion.ino
+++ modbus_portion/modbus_portion.ino
@@ -1,30 +1,25 @@
-//#include <SoftwareSerial.h>
 
-/*even parity, little endian float
+/*even parity, little endian word order Big Endian Byte order // float
    stopbit 1
 */
 
 #define MAX485_DE 3
 #define MAX485_RE_NEG 2
-
-
-
-
+// can be same pin
 
 #include <ModbusMaster.h>
 
-//SoftwareSerial mySerial(4,5);
 ModbusMaster node;
 
 const int ID = 1;
 
 uint8_t result;
 
-//uint16_t data[2];
-//
-//uint32_t value = 0;
-//
-//float reading = 0;
+uint16_t data[2];
+
+uint32_t value = 0;
+
+double reading = 0; //test with float, if no ovf then switch for memory
 
 void preTransmission()
 {
@@ -68,34 +63,28 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  result = node.readHoldingRegisters(3914, 2); // jwhether read or not
+  result = node.readHoldingRegisters(3914, 2);
   if (result == node.ku8MBSuccess) {
 
-    uint16_t data[2];
+    for (int j = 0; j < 20; j += 2) { // 0, 2, 4 6 8 10 12 14 16 18 
+      int p =0;
+      for (int i = j; i <(j+2); i += 1) {
+        data[p] = node.getResponseBuffer(i);
+        p++;
 
-    uint32_t value = 0;
+      }
+      
+      value = data[1];
+      value = value << 16;
+      value = value + data[0]; // byte shifting since Arduino processor byte order Little Endian, but PM1200 Big Endian
 
-    double reading = 0;
-
-
-    for (int i = 0; i < 2; i += 1) {
-      data[i] = node.getResponseBuffer(i);
+      reading = HexToFloat(value);
+      Serial.print("reading at buffer ");
+      Serial.print(j);
+      Serial.print(": ");
+      Serial.println(reading);
 
     }
-    Serial.print("data 1 is: ");
-    Serial.println(data[0]);
-    Serial.print("data 2 is: ");
-    Serial.println(data[1]);
-    value = data[1];
-    value = value << 16;
-    value = value + data[0];
-    Serial.print("value is: ");
-    Serial.println(value);
-
-    reading = HexToFloat(value);
-    Serial.print("reading is: ");
-    Serial.println(reading);
 
   } else {
     Serial.println("The ku8MBSuccess Flag is False");
